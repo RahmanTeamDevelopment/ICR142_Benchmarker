7c96702f8e9da60642cec3b176eacddd6df2c23d
Esty Holt <eholt@dav002.prv.davros.compute.estate>
2018-06-19 11:12:09 +0100

    Added installation messages

--- setup.sh
+++ setup.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
+echo "Installation of ICR142_Benchmarking is running ..."
+
 [ -w ".Rprofile" ] && rm .Rprofile
 
 Rscript sessionInfo.R --bootstrap-packrat > setup.log 2>&1
 
 cp packrat/packrat_source/.Rprofile ./
+
+echo "Installation of ICR142_Benchmarking has finished ..."
