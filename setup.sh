f5e7ba15be9edca12ce8e545338a5748645234f1
Esty Holt <eholt@login1.prv.davros.compute.estate>
2018-07-05 13:10:11 +0100

    changed output

--- setup.sh
+++ setup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 echo ""
-echo "Installing ICR142_Benchmarking ..."
+echo "Installing ICR142_Benchmarker ..."
 echo ""
 
 [ -w ".Rprofile" ] && rm .Rprofile
@@ -11,5 +11,5 @@ Rscript sessionInfo.R --bootstrap-packrat > setup.log 2>&1
 cp packrat/packrat_source/.Rprofile ./
 
 echo ""
-echo "Installation of ICR142_Benchmarking has finished ..."
+echo "Installation of ICR142_Benchmarker has finished ..."
 echo ""
